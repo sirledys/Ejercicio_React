aa069d73a91d1d749c7a485d013cc8780f0c31f0
sirledys <sirledys.rangel@gmail.com>
2023-05-22 16:47:42 -0500

    Variables y contador

--- src/components/Variables.jsx
+++ src/components/Variables.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Variables = () => {
+   const saludo ="Hola mundo"
+   const imagen ="https://picsum.photos/id/237/200/300"
+   const texto_alt ="Esto es una imagen"
   return (
-    <div>Variables</div>
+    <div>
+        <h1>{saludo}</h1>
+        <img src={imagen} alt={texto_alt}/>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contador from './components/Contador';
+//import Variables from './components/Variables';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+root.render( < React.StrictMode >
+
+    <
+    Contador / >
+    <
+    /React.StrictMode>  
 );
 
 // If you want to start measuring performance in your app, pass a function
